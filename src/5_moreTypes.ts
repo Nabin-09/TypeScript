1e57249e9a47b79a38d27ac6f455f38cacd73c35
Nabin <sharmanabin.0910@gmail.com>
2025-12-29 00:06:22 +0530

    void

--- src/5_moreTypes.ts
+++ src/5_moreTypes.ts
@@ -19,10 +19,33 @@ let value : any
 value = 'nabin'
 value = 2.5
 value = 67
-value.toUpperCase() //throws no error
+// value.toUpperCase()//throws no error
 
 let newValue : unknown
 newValue = 'nabin'
 newValue = 2.5
 // newValue.toUpperCase() 'newValue' is of type 'unknown' (this error is thrown)
 if(newValue === 'string') newValue.toUpperCase() // this is how we do it if used unknown
+
+
+try{
+
+}catch(error){
+    if(error instanceof Error){
+        console.log(`Error : ${error.message}`);
+    }
+    console.log('Error' , error);
+}
+
+//This is how we generally use try catch block , we ensure type of Error to be from Error class as well
+
+const data : unknown = 'Nabin is just goated'
+// const StrData : string = data //Type 'unknown' is not assignable to type 'string - although code runs its not recommended
+const StrData : string = data as string //recommended way
+console.log(StrData);
+
+
+type Role = 'admin' | 'user';
+function redirectRoleBased(role : Role): void{ //doesnt matter whats returned or nothing is returned for void
+
+}
