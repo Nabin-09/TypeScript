123d5f74ffa151f964ea4586108db8aea1ce60e6
Nabin <sharmanabin.0910@gmail.com>
2026-01-04 22:14:11 +0530

    Array of objects

--- src/10_arrays.ts
+++ src/10_arrays.ts
@@ -1,3 +1,24 @@
-const carTypes : string[] = ['SUV' , 'HatchBack' , 'Sedan']
-const carPrice : Number[] = [15 , 18 , 35]
+// const carTypes : string[] = ['SUV' , 'HatchBack' , 'Sedan']
+// const carPrice : Number[] = [15 , 18 , 35]
+
+// //Alternate way
+
+// const rating : Array<number> = [4.5, 3 , 1]
+
+
+
+type carTypes = 'SUV' | 'hatchback' | 'sedan'
+
+
+//array of objects 
+
+type Cars = {
+    name : string ,
+    price : number
+}
+
+const Car_list : Cars[] = [
+    {name : 'WagonR' , price : 5},
+    {name : 'Magnite' , price : 12}
+]
 
